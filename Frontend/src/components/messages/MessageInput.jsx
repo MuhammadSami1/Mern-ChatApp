35058a4fcca84130f82716d53a5a7b09ba33ca6b
muhammadsami <muhammadsami1242@gmail.com>
2024-12-19 01:07:51 +0500

    added: sendMessage feature complete

--- Frontend/src/components/messages/MessageInput.jsx
+++ Frontend/src/components/messages/MessageInput.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import { BsSend } from "react-icons/bs";
+import useSendMessage from "../../hooks/useSendMessage";
 
 const MessageInput = () => {
+  const [inputMessage, setInputMessage] = useState("");
+  const { loading, sendMessage } = useSendMessage();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!inputMessage) return;
+    await sendMessage(inputMessage);
+    setInputMessage("");
+  };
   return (
-    <form className="px-4 my-3">
+    <form className="px-4 my-3" onSubmit={handleSubmit}>
       <div className="w-full relative">
         <input
           type="text"
+          value={inputMessage}
+          onChange={(e) => setInputMessage(e.target.value)}
           placeholder="Send a message"
           className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white"
         />
@@ -13,7 +25,11 @@ const MessageInput = () => {
           type="submit"
           className="absolute inset-y-0 end-0 flex items-center px-3"
         >
-          <BsSend />
+          {loading ? (
+            <div className="loading loading-spinner"></div>
+          ) : (
+            <BsSend />
+          )}
         </button>
       </div>
     </form>
