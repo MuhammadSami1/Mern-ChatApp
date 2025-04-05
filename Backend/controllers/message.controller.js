9cb6d04be364885c3cff22ad1b24c11a21fdc134
muhammadsami <muhammadsami1242@gmail.com>
2025-04-05 16:54:27 +0500

    depolying

--- Backend/controllers/message.controller.js
+++ Backend/controllers/message.controller.js
@@ -1,6 +1,7 @@
 import Message from "../models/message.model.js";
 
 import Conversation from "../models/conversation.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 
 export const sendMessage = async (req, res) => {
@@ -34,6 +35,13 @@ export const sendMessage = async (req, res) => {
         // await newMessage.save()
 
         await Promise.all([conversation.save(), newMessage.save()]);
+
+        // SOCKET IO FUNCTIONALITY WILL GO HERE
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if (receiverSocketId) {
+            // io.to(<socket_id>).emit() used to send events to specific client
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
 
         res.status(201).json(newMessage)
 
--- Frontend/src/components/siderbar/Conversation.jsx
+++ Frontend/src/components/siderbar/Conversation.jsx
@@ -1,18 +1,22 @@
+import { useSocketContext } from "../../context/SocketContext";
 import useConversation from "../../store/useConversation";
 
-const Conversation = ({ conversation, emoji, lastIndex }) => {
+const Conversation = ({ conversation, lastIdx, emoji }) => {
   const { selectedConversation, setSelectedConversation } = useConversation();
 
   const isSelected = selectedConversation?._id === conversation._id;
+  const { onlineUsers } = useSocketContext();
+  const isOnline = onlineUsers.includes(conversation._id);
+
   return (
     <>
       <div
-        className={`flex gap-2 items-center hover:bg-slate-500 rounded p-2 py-1 cursor-pointer ${
-          isSelected ? "bg-sky-500" : ""
-        }`}
+        className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+				${isSelected ? "bg-sky-500" : ""}
+			`}
         onClick={() => setSelectedConversation(conversation)}
       >
-        <div className="avatar online">
+        <div className={`avatar ${isOnline ? "online" : ""}`}>
           <div className="w-12 rounded-full">
             <img src={conversation.profilePic} alt="user avatar" />
           </div>
@@ -26,10 +30,8 @@ const Conversation = ({ conversation, emoji, lastIndex }) => {
         </div>
       </div>
 
-      {/* Divider */}
-      {!lastIndex && <div className="divider my-0 py-0 h-1" />}
+      {!lastIdx && <div className="divider my-0 py-0 h-1" />}
     </>
   );
 };
-
 export default Conversation;
